5ea80109c3b9c585c3aa286925f34b5f357dabdf
chandni-khan <Chandni.Khan@bbd.co.za>
2024-05-08 11:58:35 +0530

    web config by chandni

--- src/main/java/com/example/helloworld/helloworld/config/WebConfig.java
+++ src/main/java/com/example/helloworld/helloworld/config/WebConfig.java
@@ -1,21 +1,18 @@
 package com.example.helloworld.helloworld.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry.addMapping("/getIncome")
                 .allowedOrigins("http://127.0.0.1:5500")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .exposedHeaders("Access-Control-Allow-Origin")
-                .allowCredentials(true)
-                .maxAge(3600);
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
