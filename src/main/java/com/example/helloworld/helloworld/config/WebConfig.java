ba63b5d9c353de5aa2794837f1496dbf6450d647
chandni-khan <Chandni.Khan@bbd.co.za>
2024-05-08 10:13:41 +0530

    web config by chandni

--- src/main/java/com/example/helloworld/helloworld/config/WebConfig.java
+++ src/main/java/com/example/helloworld/helloworld/config/WebConfig.java
@@ -3,14 +3,15 @@ package com.example.helloworld.helloworld.config;
  
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*") // Allow requests from any origin
-                .allowedMethods("GET", "POST", "PUT", "DELETE"); // Allow only specific methods
+        registry.addMapping("/**");
     }
 }
+
