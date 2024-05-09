a2a73660767dad4f869e146c9fa616767b84dd9d
chandni-khan <Chandni.Khan@bbd.co.za>
2024-05-09 17:06:16 +0530

    config by chandni

--- src/main/java/com/example/helloworld/helloworld/config/WebConfig.java
+++ src/main/java/com/example/helloworld/helloworld/config/WebConfig.java
@@ -13,6 +13,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://127.0.0.1:5500")
-                .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS");
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
