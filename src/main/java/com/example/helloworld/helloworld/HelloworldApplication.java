9bcb6df622b0b0deffe1b28fe32e15a9e979d2df
chandni-khan <Chandni.Khan@bbd.co.za>
2024-05-09 12:58:34 +0530

    web config by chandni

--- src/main/java/com/example/helloworld/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/helloworld/HelloworldApplication.java
@@ -2,8 +2,10 @@ package com.example.helloworld.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example.helloworld", "com.example.helloworld.helloworld.config"})
 public class HelloworldApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloworldApplication.class, args);
